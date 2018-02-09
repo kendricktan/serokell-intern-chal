e632b7c9052618a08252efbf50c2f0f5bb8fbb91
Kendrick Tan <kendricktan0814@gmail.com>
2018-02-10 08:20:29 +1000

    more boilerplate

--- src/Serokell/Node.hs
+++ src/Serokell/Node.hs
@@ -3,6 +3,37 @@
 
 module Serokell.Node () where
 
-
+import           Data.Binary                (Binary (..), decode, encode)
+import           Data.Set                   as Set
+import           Numeric.Natural            (Natural)
 import           Serokell.Communication.IPC (Conversation (..), NodeId (..),
-                                             connectToUnixSocket, listenUnixSocket)
+                                             connectToUnixSocket,
+                                             listenUnixSocket)
+
+import qualified Crypto.Sign.Ed25519        as ECC
+import qualified Data.ByteString.Lazy       as LByteString
+import           Data.Semigroup             ((<>))
+
+
+-- Simple transaction data structure
+data Tx = Tx { txFrom   :: NodeId
+             , txTo     :: NodeId
+             , txAmount :: Natural
+             , txSig    :: LByteString.ByteString
+             } deriving (Eq, Ord)
+
+-- Instructions on how to convert it to bytestring
+instance Binary Tx where
+    put t = put (txFrom t) <> put (txTo t) <> put (txAmount t) <> put (txSig t)
+
+    get = Tx <$> get <*> get <*> get <*> get
+
+-- Node Environment, Immutable
+data NodeEnvironment = NodeEnvironment
+    { nodeEnvId      :: NodeId
+    , nodeEnvPrivKey :: ECC.SecretKey
+    , nodeEnvPubKey  :: ECC.PublicKey
+    }
+
+-- Mutable part of the node state
+data NodeStorage = NodeStorage { nodeStorageTxs :: SetTx }
