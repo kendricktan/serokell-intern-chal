dd51f04cea63eaf557481be309fba14bc77401d1
Kendrick Tan <kendricktan0814@gmail.com>
2018-02-12 18:29:15 +1000

    revamp

--- src/Serokell/Node.hs
+++ src/Serokell/Node.hs
@@ -4,32 +4,33 @@
 
 module Serokell.Node where
 
-import           Control.Concurrent         (forkIO)
-import           Control.Monad              (void)
-import           Control.Monad.Reader       (ReaderT, runReaderT)
-import           Control.Monad.State        (StateT, get, put, runStateT)
-import           Control.Monad.Trans        (lift)
-import           Control.Monad.Trans.Except (ExceptT, runExceptT)
-import           Data.ByteString.Char8      (pack)
-import           Data.ByteString.Lazy       (ByteString, fromStrict, toStrict)
-import           Data.ByteString.Lazy.UTF8  (fromString, toString)
-import           Data.Either                (Either, either)
-import           Data.Semigroup             ((<>))
-import           Data.Set                   (Set, insert)
-import           GHC.Generics               (Generic)
-import           Numeric.Natural            (Natural)
-import           Serokell.Communication.IPC (Conversation (..), NodeId (..),
-                                             connectToUnixSocket,
-                                             listenUnixSocket)
-import           System.IO                  (hPrint, hPutStrLn, stderr)
+import           Control.Concurrent          (forkIO)
+import           Control.Monad               (void)
+import           Control.Monad.Reader        (ReaderT, runReaderT)
+import           Control.Monad.State         (StateT, get, put, runStateT)
+import           Control.Monad.Trans         (lift)
+import           Control.Monad.Trans.Except  (ExceptT, runExceptT)
+import           Data.Either                 (Either, either)
+import           Data.Semigroup              ((<>))
+import           GHC.Generics                (Generic)
+import           Numeric.Natural             (Natural)
+import           Serokell.Communication.IPC  (Conversation (..), NodeId (..),
+                                              connectToUnixSocket,
+                                              listenUnixSocket)
+import           System.IO                   (hPrint, hPutStrLn, stderr)
 
-import qualified Crypto.Hash.SHA256         as SHA256
-import qualified Crypto.Sign.Ed25519        as ECC
-import qualified Data.Aeson                 as Aeson
-import qualified Data.Binary                as Binary
-import qualified Data.HashMap               as HM
-import qualified Data.Text                  as Text
-import qualified Data.Text.IO               as TextIO
+import qualified Crypto.Hash.SHA256          as SHA256
+import qualified Crypto.Sign.Ed25519         as ECC
+import qualified Data.Aeson                  as Aeson
+import qualified Data.Binary                 as Binary
+import qualified Data.ByteString             as BString
+import qualified Data.ByteString.Base16      as B16
+import qualified Data.ByteString.Base16.Lazy as B16L
+import qualified Data.ByteString.Char8       as C8
+import qualified Data.HashMap                as HM
+import qualified Data.Set                    as Set
+import qualified Data.Text                   as Text
+import qualified Data.Text.IO                as TextIO
 
 -- TxAction Monad
 -- e = Environment
@@ -46,10 +47,14 @@ evalTxAction action env0 state0 = do
 
 
 -- Blockchain Primitives
-data Tx = Tx { txHash   :: Maybe String
-             , txIn     :: String
-             , txOut    :: String
-             , txAmount :: Natural
+type Hash    = String
+type Address = String
+
+data Tx = Tx { txHash   :: Hash     -- Hash of the tx
+             , txSig    :: Hash     -- Signature of the tx
+             , txFrom   :: Address  -- Who are we sending it to
+             , txTo     :: Address  -- Who is receiving it
+             , txAmount :: Natural  -- How much
              } deriving (Eq, Ord, Generic, Show)
 
 -- Error Handling
@@ -69,23 +74,74 @@ instance Aeson.FromJSON Tx
 
 -- Node Environment, Immutable
 data NodeEnvironment = NodeEnvironment
-    { nodeEnvId      :: NodeId
-    , nodeEnvPrivKey :: ECC.SecretKey
-    , nodeEnvPubKey  :: ECC.PublicKey
+    { nodeEnvId     :: NodeId
+    , nodeEnvSecKey :: ECC.SecretKey
+    , nodeEnvPubKey :: ECC.PublicKey
     }
 
 -- Cache State (Keeping track of all the UTXOs)
-data TxState = TxState { txs    :: Set Tx
-                       , txsMap :: HM.Map String Tx
-                       , utxos  :: HM.Map String Natural
+data TxState = TxState { txs   :: Set.Set Tx
+                       , txsm  :: HM.Map Hash Tx
+                       , utxos :: HM.Map Address Natural
                        } deriving Show
 
 -- DRY
 type TxMonad a = TxAction NodeEnvironment TxState a
 
--- Tx helper functions
-createTx :: String -> String -> String -> Either TxError Tx
-createTx privKey pubKey amnt = undefined
+-- Helper functions
+
+-- Base16 encode
+b16e x = C8.unpack $ B16.encode x
+
+-- Base16 decode
+b16d x = B16.decode $ C8.pack x
+
+-- Base16 encode public keys
+b16pk :: ECC.PublicKey -> String
+b16pk k = b16e $ ECC.unPublicKey k
+
+-- Base16 encode secret keys
+b16sk :: ECC.SecretKey -> String
+b16sk k = b16e $ ECC.unSecretKey k
+
+-- Base16 decode public key
+b16dpk :: String -> ECC.PublicKey
+b16dpk s = ECC.PublicKey $ (fst . b16d) s
+
+-- Base16 decode secret key
+b16dsk :: String -> ECC.SecretKey
+b16dsk s = ECC.SecretKey $ (fst . b16d) s
+
+-- Get address (public key) from secret key
+getSKAddress :: ECC.SecretKey -> String
+getSKAddress = b16pk . ECC.toPublicKey
+
+-- Get hash of a tx
+getTxHashBString :: Tx -> BString.ByteString
+getTxHashBString (Tx _ _ f t a) = SHA256.hash $ C8.pack $ concat $ show <$> [f, t, show a]
+
+getTxHash :: Tx -> Hash
+getTxHash = C8.unpack . getTxHashBString
+
+-- Sign a tx
+signTx :: Tx -> ECC.SecretKey -> String
+signTx tx sk = b16e $ ECC.sign sk (getTxHashBString tx)
+
+-- Is the tx valid?
+-- Checks if we have enough , then checks the signature
+verifyTx :: Tx -> TxState -> Bool
+verifyTx tx txstate = case HM.lookup (txFrom tx) (utxos txstate) of
+                        Nothing -> False
+                        Just x  -> case x < (txAmount tx) of
+                                     False -> False
+                                     True  -> ECC.verify (b16dpk (txFrom tx)) (getTxHashBString tx)
+
+createTx :: String -> String -> String -> Tx
+createTx sk txto amnt = Tx txhash txsig txfr txto (read amnt :: Natural)
+    where txfr = getSKAddress (b16dsk sk)
+          txtmp = Tx "" "" txfr txto (read amnt :: Natural)
+          txhash = getTxHash txtmp
+          txsig = signTx txtmp (b16dsk sk)
 
 -- Lens maybe?
 lift2 a = lift $ lift a
@@ -100,7 +156,8 @@ nodeHandler c = do
     input <- lift3 TextIO.getLine
     case Text.words input of
       ("SUBMIT" : privKey : pubKey : amnt) -> undefined
-      ("QUERY": _)   -> undefined
-      _              -> do
+      ("QUERY": txhash)                    -> undefined
+      ("BALANCE": pubKey)                  -> undefined
+      _                                    -> do
           lift3 $ hPrint stderr $ "Invalid command: " <> input
           nodeHandler c
